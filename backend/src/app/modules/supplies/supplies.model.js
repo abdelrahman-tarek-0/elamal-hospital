65630bc9a29c0592c189e0c1a32470a81c29c1e9
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-10-05 18:24:14 +0300

    [UPDATE] Supply model is done

---
+++ backend/src/app/modules/supplies/supplies.model.js
@@ -0,0 +1,80 @@
+const { DataTypes, Model } = require('sequelize')
+const connection = require('../../../config/database.config')
+
+class Supply extends Model {
+   static async getAllSupply() {
+      return await Supply.findAll()
+   }
+
+   static async getSupplyById(id) {
+      return await Supply.findByPk(id)
+   }
+
+   static async createSupply(supply) {
+      return await Supply.create(supply)
+   }
+
+   static async updateSupply(id, supply) {
+      return await Supply.update(supply, {
+         where: {
+            id,
+         },
+      })
+   }
+
+   static async deleteSupply(id) {
+      return await Supply.destroy({
+         where: {
+            id,
+         },
+      })
+   }
+}
+
+Supply.init(
+   {
+      // medical supply
+
+      id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+         autoIncrement: true,
+      },
+
+      name: {
+         type: DataTypes.STRING(255),
+         allowNull: false,
+      },
+
+      description: {
+         type: DataTypes.STRING(4095),
+         allowNull: true,
+         defaultValue: '',
+      },
+
+      price: {
+         type: DataTypes.DECIMAL(10, 2),
+         allowNull: false,
+         defaultValue: 0.0,
+      },
+
+      stock: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         defaultValue: 0,
+      },
+
+      image: {
+         type: DataTypes.STRING(255),
+         allowNull: true,
+         defaultValue: '',
+      },
+   },
+   {
+      sequelize: connection,
+      modelName: 'Supply',
+   }
+)
+
+module.exports = Supply
