427a9e13f7576d96b5857e24cd1644e43b9e1e2e
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-10-05 18:59:02 +0300

    [UPDATE] Supply route is done

--- backend/src/app/routes/index.routes.js
+++ backend/src/app/routes/index.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require('express')
 const resBuilder = require('../utils/response.builder')
+const suppliesRouter = require('../modules/supplies/supplies.routes')
 
 const router = Router()
 
 router.get('/', (req, res) => {
    resBuilder(res, 200, 'Welcome to the API')
 })
+
+router.use('/supplies', suppliesRouter)
 
 module.exports = router
