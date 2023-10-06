f6c98dec2e9a205deb6dfc8587c7ebf43979a688
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-10-06 15:17:47 +0300

    [UPDATE] simple nav bar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
+import ResponsiveAppBar from './components/NavBar'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <header className="App-header">
+            <ResponsiveAppBar />
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+         </header>
+      </div>
+   )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,201 @@
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+
+import HomeIcon from '@mui/icons-material/Home'
+import AccessibilityIcon from '@mui/icons-material/Accessibility'
+import BadgeIcon from '@mui/icons-material/Badge'
+
+const pages = [
+   {
+      name: 'الرئيسية',
+      path: '/',
+      icon: HomeIcon,
+   },
+   {
+      name: 'المرضى',
+      path: '/patients',
+      icon: AccessibilityIcon,
+   },
+   {
+      name: 'الموظفين',
+      path: '/employees',
+      icon: BadgeIcon,
+   },
+]
+
+function ResponsiveAppBar() {
+   const [anchorElNav, setAnchorElNav] = React.useState(null)
+
+   const handleOpenNavMenu = (event) => {
+      setAnchorElNav(event.currentTarget)
+   }
+
+   const handleCloseNavMenu = () => {
+      setAnchorElNav(null)
+   }
+
+   return (
+      <AppBar
+         position="static"
+         sx={{
+            backgroundColor: '#200100',
+            color: '#98f701',
+         }}
+      >
+         <Container
+            maxWidth="xl"
+            sx={{
+               maxWidth: '100% !important',
+            }}
+         >
+            <Toolbar
+               disableGutters
+               sx={{
+                  marginRight: '20px',
+               }}
+            >
+               <Box
+                  component="img"
+                  sx={{
+                     height: 48,
+                     display: { xs: 'none', md: 'flex' },
+                  }}
+                  alt="Your logo."
+                  src="http://localhost:3000/logo.jpg"
+               />
+               <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  href="/"
+                  sx={{
+                     mr: 2,
+                     display: { xs: 'none', md: 'flex' },
+                     fontFamily: 'monospace',
+                     fontWeight: 700,
+                     color: 'inherit',
+                     textDecoration: 'none',
+                     marginLeft: '100px',
+                  }}
+               >
+                  مستشفي الأمل
+               </Typography>
+
+               <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                  <IconButton
+                     size="large"
+                     aria-label="account of current user"
+                     aria-controls="menu-appbar"
+                     aria-haspopup="true"
+                     onClick={handleOpenNavMenu}
+                     color="inherit"
+                  >
+                     <MenuIcon />
+                  </IconButton>
+                  <Menu
+                     id="menu-appbar"
+                     anchorEl={anchorElNav}
+                     anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                     }}
+                     keepMounted
+                     transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                     }}
+                     open={Boolean(anchorElNav)}
+                     onClose={handleCloseNavMenu}
+                     sx={{
+                        display: { xs: 'block', md: 'none' },
+                     }}
+                  >
+                     {pages.map((page) => (
+                        <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                           <Typography textAlign="center">
+                              {page.name}
+                           </Typography>
+                        </MenuItem>
+                     ))}
+                  </Menu>
+               </Box>
+
+               <Box
+                  component="img"
+                  sx={{
+                     height: 48,
+                     display: { xs: 'flex', md: 'none' },
+                  }}
+                  alt="Your logo."
+                  src="http://localhost:3000/logo.jpg"
+               />
+
+               <Typography
+                  variant="h5"
+                  noWrap
+                  component="a"
+                  href="#app-bar-with-responsive-menu"
+                  sx={{
+                     mr: 2,
+                     display: { xs: 'flex', md: 'none' },
+                     flexGrow: 1,
+                     fontFamily: 'monospace',
+                     fontWeight: 700,
+                     color: 'inherit',
+                     textDecoration: 'none',
+                  }}
+               >
+                  مستشفي الأمل
+               </Typography>
+
+               <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                  {pages.map((page) => (
+                     <Button
+                        key={page.name}
+                        onClick={handleCloseNavMenu}
+                        sx={{
+                           my: 2,
+                           color: 'inherit',
+                           display: 'block',
+                           margin: '0 16px',
+                           fontSize: '1.5rem',
+                           ':hover': {
+                              background: '#365c00',
+                           },
+                        }}
+                     >
+                        <page.icon
+                           sx={{
+                              fontSize: '1.2rem',
+                              marginLeft: '5px',
+                           }}
+                        />
+
+                        <Typography
+                           variant="span"
+                           sx={{
+                              fontSize: '1.5rem',
+                           }}
+                        >
+                           {page.name}
+                        </Typography>
+                     </Button>
+                  ))}
+               </Box>
+
+               <Box sx={{ flexGrow: 0 }}></Box>
+            </Toolbar>
+         </Container>
+      </AppBar>
+   )
+}
+export default ResponsiveAppBar
